4b38be6c5c5fcf6dd516dc93ff9d405df04bff9f
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2025-09-06 09:36:35 +0530

    add registration using Aadhaar Card Number

--- add_faces.py
+++ add_faces.py
@@ -24,9 +24,51 @@ while True:
 
     for (x,y,w,h) in faces:
         crop_img = frame[y:y+h, x:x+w]
+        resized_img = cv2.resize(crop_img, (50, 50))
+
+        if len(faces_data) <= framesTotal and i%captureAfterFrame == 0:
+            faces_data.append(resized_img)
+        
+        i+=1
+        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 1)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (50, 50, 255), 1)
         
 
     cv2.imshow('frame', frame)
     k=cv2.waitKey(1)
     if k==ord('q') or len(faces_data) >= framesTotal:
         break
+
+
+video.release()
+cv2.destroyAllWindows()
+
+print(len(faces_data))
+
+faces_data = np.array(faces_data)
+faces_data = faces_data.reshape((framesTotal, -1))
+print(faces_data)
+
+
+if "names.pkl" not in os.listdir('data/'):
+    names = [name]*framesTotal
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+else:
+    with open('data/names.pkl', 'rb') as f:
+        names = pickle.load(f)
+    names = names + [name]*framesTotal
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+
+
+
+if "faces_data.pkl" not in os.listdir('data/'):
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+else:
+    with open('data/faces_data.pkl', 'rb') as f:
+        faces = pickle.load(f)
+    faces = np.append(faces, faces_data, axis=0)
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces, f)
